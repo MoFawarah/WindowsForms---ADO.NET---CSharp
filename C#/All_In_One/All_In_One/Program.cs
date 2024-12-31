243e5cc0ee8ad4e4b6ca45d9c5d64b26783b4c8e
M.Fawareh <M.Fawareh@MOH-FAWAREH>
2024-12-31 16:59:57 +0300

    updated

---
+++ C#/All_In_One/All_In_One/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//ProgrammingAdvices.com
+//Mohammed Abu-Hadhoud
+
+using System;
+
+namespace Main
+{
+    enum enGender
+    {
+        Male,
+        Female
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+        var User1 = new
+        {
+            FirstName = "Abdullrahman",
+            FatherName = "Abdullah",
+            LastName = "Al-Akhzami",
+            Age = 21,
+            Gender = enGender.Male
+        };
+
+        string InfoCard1 = $"\n\n{(User1.Gender == enGender.Male ? "Mr." : "Ms.")} {User1.FirstName} {(User1.Gender == enGender.Male ? "Ben" : "Bent")} {User1.FatherName} {User1.LastName}, It was {User1.Age}";
+        string InfoCard2 = $"{(User1.Gender == enGender.Female? "Ms":"Mr")}";
+
+        Console.WriteLine(InfoCard1);
+
+        }
+    }
+}
+
---
+++ C#/FirstWinFormsProject/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinFormsProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        //reset
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text = "";
+            textBox1.Enabled = true; 
+            textBox1.Visible = true;
+            this.Text = "Weeeeeee";
+            button2.Visible = false;
+            button3.Enabled = false;
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            textBox2.Enabled = true;
+            textBox2.Width = 200;
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+          
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
